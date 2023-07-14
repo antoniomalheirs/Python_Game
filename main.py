69025ce072808725db50cefe61579add8c79575f
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-14 04:36:36 -0300

    variáveis Player/Enemy

--- main.py
+++ main.py
@@ -8,16 +8,30 @@ from pygame import mixer
 pygame.init()
 
 # Criando tela do jogo
-screen = pygame.display.set_mode((600, 250))
-
-# Background
-background = pygame.image.load('background.png')
-
-# Sound
-mixer.music.load("background.wav")
-mixer.music.play(-1)
+screen = pygame.display.set_mode((800, 600))
 
 # Descrição e Ícone
 pygame.display.set_caption("Space Invaders python version")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
+
+# Som
+mixer.music.load("background.wav")
+mixer.music.play(-1)
+
+# Fundo
+background = pygame.image.load('background.png')
+
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# Enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
