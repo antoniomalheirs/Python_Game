63721c98264d576e97a27b35862842d6e0e04036
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-14 04:45:40 -0300

    Spawn inimigos/movimentação das balas/Pontuação e Game Over

--- main.py
+++ main.py
@@ -8,30 +8,55 @@ from pygame import mixer
 pygame.init()
 
 # Criando tela do jogo
-screen = pygame.display.set_mode((800, 600))
+tela = pygame.display.set_mode((800, 600))
 
 # Descrição e Ícone
 pygame.display.set_caption("Space Invaders python version")
-icon = pygame.image.load('ufo.png')
-pygame.display.set_icon(icon)
+icone = pygame.image.load('ufo.png')
+pygame.display.set_icon(icone)
 
 # Som
 mixer.music.load("background.wav")
 mixer.music.play(-1)
 
 # Fundo
-background = pygame.image.load('background.png')
+fundotela = pygame.image.load('background.png')
 
 # Player
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
-playerX_change = 0
+playerX_alt = 0
 
-# Enemy
+# Inimigos
 enemyImg = []
 enemyX = []
 enemyY = []
-enemyX_change = []
-enemyY_change = []
-num_of_enemies = 6
+enemyX_alt = []
+enemyY_alt = []
+num_de_inimigos = 10
+
+for i in range(num_de_inimigos):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_alt.append(4)
+    enemyY_alt.append(40)
+
+# Balas
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_alt = 0
+bulletY_alt = 10
+bullet_estado = "pronto"
+
+# Pontuação
+pontuvalor = 0
+fonte = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+testY = 10
+
+# Game Over
+gameoverfonte = pygame.font.Font('freesansbold.ttf', 64)
