e6367fc3b52ea72b7c4de1f5c128fdefa3b6b603
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-14 04:24:58 -0300

    informações basicas do jogo

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ import random
 import pygame
 from pygame import mixer
 
-#Iniciando Pygame
+# Iniciando Pygame
 pygame.init()
 
-#Criando tela do jogo
-screen = pygame.display.set_mode((800, 600))
+# Criando tela do jogo
+screen = pygame.display.set_mode((600, 250))
+
+# Background
+background = pygame.image.load('background.png')
+
+# Sound
+mixer.music.load("background.wav")
+mixer.music.play(-1)
+
+# Descrição e Ícone
+pygame.display.set_caption("Space Invaders python version")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
